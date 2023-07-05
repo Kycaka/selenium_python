34b0d5731ba6b6106ccffcbcee4e809b44a365b1
Renat <spamhatertom@gmail.com>
2023-07-05 10:46:46 +0300

    initial

---
+++ chrome_driver/main.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+import time
+import random
+
+base_url = 'https://www.saucedemo.com'
+
+service = Service(
+    executable_path=r'F:\Dev\selenium_python\chrome_driver\chromedriver.exe'
+)
+
+users_list = [
+    'standard_user',
+    'locked_out_user',
+    'problem_user',
+    'performance_glitch_user'
+]
+
+with webdriver.Chrome(service=service) as driver:
+
+    try:
+        driver.get(url=base_url)
+
+        user_name = driver.find_element(By.XPATH, '//input[@id="user-name"]')
+        user_name.send_keys(f'{random.choice(users_list)}')
+
+        password = driver.find_element(By.CSS_SELECTOR, '#password')
+        password.send_keys('secret_sauce')
+
+        login_button = driver.find_element(
+            By.XPATH,
+            '//input[@id="login-button"]'
+        )
+        login_button.click()
+
+        time.sleep(3)
+    except Exception as exeption:
+        print(exeption)
+    finally:
+        driver.close()
+        driver.quit()
